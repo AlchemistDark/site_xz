920c06a019b5a43402d1b86a4ee63df21851adf5
AlchemistDark <justplus@rambler.ru>
2022-10-14 10:10:28 +0700

    First commit.

---
+++ lib/mok.dart
@@ -0,0 +1,302 @@
+import 'package:flutter/material.dart';
+
+
+class Group1Widget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // Figma Flutter Generator Group1Widget - GROUP
+    return Container(
+        width: 375,
+        height: 812,
+
+        child: Stack(
+            children: <Widget>[
+              Positioned(
+                  top: 0,
+                  left: 0,
+                  child: Container(
+                      width: 375,
+                      height: 812,
+                      decoration: BoxDecoration(
+                        borderRadius : BorderRadius.only(
+                          topLeft: Radius.circular(30),
+                          topRight: Radius.circular(30),
+                          bottomLeft: Radius.circular(30),
+                          bottomRight: Radius.circular(30),
+                        ),
+                        color : Color.fromRGBO(46, 49, 58, 1),
+                      ),
+                      child: Stack(
+                          children: <Widget>[
+                            Positioned(
+                                top: 111,
+                                left: 16,
+                                child: Container(
+                                  decoration: BoxDecoration(
+
+                                  ),
+                                  padding: EdgeInsets.symmetric(horizontal: 0, vertical: 0),
+                                  child: Column(
+                                    mainAxisSize: MainAxisSize.min,
+
+                                    children: <Widget>[null, SizedBox(height : 9),
+                                      null, SizedBox(height : 9),
+                                      null, SizedBox(height : 9),
+                                      null, SizedBox(height : 9),
+                                      null, SizedBox(height : 9),
+                                      null, SizedBox(height : 9),
+                                      null, SizedBox(height : 9),
+                                      null, SizedBox(height : 9),
+                                      null, SizedBox(height : 9),
+                                      null, SizedBox(height : 9),
+                                      null, SizedBox(height : 9),
+                                      null,
+                                    ],
+                                  ),
+                                )
+                            ),Positioned(
+                                top: 95,
+                                left: 75,
+                                child: SvgPicture.asset(
+                                    'assets/images/vector50.svg',
+                                    semanticsLabel: 'vector50'
+                                );
+                            ),Positioned(
+                                top: 703,
+                                left: 0,
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                    color : Color.fromRGBO(41, 43, 51, 1),
+                                  ),
+                                  padding: EdgeInsets.symmetric(horizontal: 16, vertical: 4),
+                                  child: Row(
+                                    mainAxisSize: MainAxisSize.min,
+
+                                    children: <Widget>[
+                                      null, SizedBox(width : 11),
+                                      null, SizedBox(width : 11),
+                                      null, SizedBox(width : 11),
+                                      null, SizedBox(width : 11),
+                                      null,
+
+                                    ],
+                                  ),
+                                )
+                            ),Positioned(
+                                top: 788,
+                                left: 0,
+                                child: null
+                            ),Positioned(
+                                top: 0,
+                                left: 0,
+                                child: Container(
+                                    width: 375,
+                                    height: 93,
+                                    decoration: BoxDecoration(
+                                      color : Color.fromRGBO(40, 42, 49, 1),
+                                    ),
+                                    child: Stack(
+                                        children: <Widget>[
+                                          Positioned(
+                                              top: 0,
+                                              left: 0,
+                                              child: null
+                                          ),Positioned(
+                                              top: 47,
+                                              left: 16,
+                                              child: Container(
+                                                  width: 40,
+                                                  height: 40,
+                                                  decoration: BoxDecoration(
+                                                    color : Color.fromRGBO(255, 255, 255, 1),
+                                                  ),
+                                                  child: Stack(
+                                                      children: <Widget>[
+                                                        Positioned(
+                                                            top: 0,
+                                                            left: 0,
+                                                            child: Container(
+                                                                width: 40,
+                                                                height: 40,
+                                                                decoration: BoxDecoration(
+                                                                  boxShadow : [BoxShadow(
+                                                                      color: Color.fromRGBO(31, 33, 42, 1),
+                                                                      offset: Offset(2,2),
+                                                                      blurRadius: 5
+                                                                  )],
+                                                                  gradient : LinearGradient(
+                                                                      begin: Alignment(0.578136146068573,0.8181171417236328),
+                                                                      end: Alignment(-0.8181171417236328,0.578136146068573),
+                                                                      colors: [Color.fromRGBO(70, 72, 81, 1),Color.fromRGBO(40, 43, 51, 1)]
+                                                                  ),
+                                                                  borderRadius : BorderRadius.all(Radius.elliptical(40, 40)),
+                                                                )
+                                                            )
+                                                        ),Positioned(
+                                                            top: 5,
+                                                            left: 5,
+                                                            child: Container(
+                                                                width: 30,
+                                                                height: 30,
+                                                                decoration: BoxDecoration(
+                                                                  color : Color.fromRGBO(255, 255, 255, 1),
+                                                                ),
+                                                                child: Stack(
+                                                                    children: <Widget>[
+                                                                      Positioned(
+                                                                          top: 5.5,
+                                                                          left: 8.5,
+                                                                          child: SvgPicture.asset(
+                                                                              'assets/images/vectorstroke.svg',
+                                                                              semanticsLabel: 'vectorstroke'
+                                                                          );
+                                                                      ),
+                                                                    ]
+                                                                )
+                                                            )
+                                                        ),
+                                                      ]
+                                                  )
+                                              )
+                                          ),Positioned(
+                                              top: 57,
+                                              left: 56,
+                                              child: Text('Содержание номера', textAlign: TextAlign.center, style: TextStyle(
+                                                  color: Color.fromRGBO(255, 255, 255, 1),
+                                                  fontFamily: 'Roboto',
+                                                  fontSize: 18,
+                                                  letterSpacing: 0 /*percentages not used in flutter. defaulting to zero*/,
+                                                  fontWeight: FontWeight.normal,
+                                                  height: 1
+                                              ),)
+                                          ),Positioned(
+                                              top: 47,
+                                              left: 319,
+                                              child: Container(
+                                                  width: 40,
+                                                  height: 40,
+                                                  decoration: BoxDecoration(
+                                                    color : Color.fromRGBO(255, 255, 255, 1),
+                                                  ),
+                                                  child: Stack(
+                                                      children: <Widget>[
+                                                        Positioned(
+                                                            top: 0,
+                                                            left: 0,
+                                                            child: Container(
+                                                                width: 40,
+                                                                height: 40,
+                                                                decoration: BoxDecoration(
+                                                                  boxShadow : [BoxShadow(
+                                                                      color: Color.fromRGBO(31, 33, 42, 1),
+                                                                      offset: Offset(2,2),
+                                                                      blurRadius: 5
+                                                                  )],
+                                                                  gradient : LinearGradient(
+                                                                      begin: Alignment(0.578136146068573,0.8181171417236328),
+                                                                      end: Alignment(-0.8181171417236328,0.578136146068573),
+                                                                      colors: [Color.fromRGBO(70, 72, 81, 1),Color.fromRGBO(40, 43, 51, 1)]
+                                                                  ),
+                                                                  borderRadius : BorderRadius.all(Radius.elliptical(40, 40)),
+                                                                )
+                                                            )
+                                                        ),Positioned(
+                                                            top: 8,
+                                                            left: 8,
+                                                            child: Container(
+                                                                width: 24,
+                                                                height: 24,
+                                                                decoration: BoxDecoration(
+                                                                  color : Color.fromRGBO(255, 255, 255, 1),
+                                                                ),
+                                                                child: Stack(
+                                                                    children: <Widget>[
+                                                                      Positioned(
+                                                                          top: 0,
+                                                                          left: 0,
+                                                                          child: SvgPicture.asset(
+                                                                              'assets/images/vector.svg',
+                                                                              semanticsLabel: 'vector'
+                                                                          );
+                                                                      ),
+                                                                    ]
+                                                                )
+                                                            )
+                                                        ),
+                                                      ]
+                                                  )
+                                              )
+                                          ),
+                                        ]
+                                    )
+                                )
+                            ),
+                          ]
+                      )
+                  )
+              ),
+            ]
+        )
+    );
+  }
+}
+class ArrowWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // Figma Flutter Generator ArrowWidget - FRAME
+    return Container(
+        width: 40,
+        height: 40,
+        decoration: BoxDecoration(
+          color : Color.fromRGBO(255, 255, 255, 1),
+        ),
+        child: Stack(
+            children: <Widget>[
+              Positioned(
+                  top: 0,
+                  left: 0,
+                  child: Container(
+                      width: 40,
+                      height: 40,
+                      decoration: BoxDecoration(
+                        boxShadow : [BoxShadow(
+                            color: Color.fromRGBO(31, 33, 42, 1),
+                            offset: Offset(2,2),
+                            blurRadius: 5
+                        )],
+                        gradient : LinearGradient(
+                            begin: Alignment(0.578136146068573,0.8181171417236328),
+                            end: Alignment(-0.8181171417236328,0.578136146068573),
+                            colors: [Color.fromRGBO(70, 72, 81, 1),Color.fromRGBO(40, 43, 51, 1)]
+                        ),
+                        borderRadius : BorderRadius.all(Radius.elliptical(40, 40)),
+                      )
+                  )
+              ),Positioned(
+                  top: 5,
+                  left: 5,
+                  child: Container(
+                      width: 30,
+                      height: 30,
+                      decoration: BoxDecoration(
+                        color : Color.fromRGBO(255, 255, 255, 1),
+                      ),
+                      child: Stack(
+                          children: <Widget>[
+                            Positioned(
+                                top: 5.5,
+                                left: 8.5,
+                                child: SvgPicture.asset(
+                                    'assets/images/vectorstroke.svg',
+                                    semanticsLabel: 'vectorstroke'
+                                );
+                            ),
+                          ]
+                      )
+                  )
+              ),
+            ]
+        )
+    );
+  }
+}
